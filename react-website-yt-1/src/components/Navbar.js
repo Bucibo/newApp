7bf8021236d470e853b930692f8aaca72b0c5fe0
Bucibo Neo <BCBNEO001@MYUCT.AC.ZA>
2025-07-03 05:28:06 +0200

    Added navbar final features

--- react-website-yt-1/src/components/Navbar.js
+++ react-website-yt-1/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -18,35 +18,40 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton()
+    }, []);
+
     window.addEventListener('resize', showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navBar-logo">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             BNH <i className="fab fa-typo3"></i>
             </Link>
-            <div classNamw='menu-icon' onClick={handleClick}>
+            <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li className='nav-item'>
-                    <Link to='/' className='nav-links onClick{closeMobileMenu}'>
+                    <Link to='/' className='nav-links' onClick=
+                    {closeMobileMenu}>
                         Home
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/services' className='nav-links onClick{closeMobileMenu}'>
+                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                         Services
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/products' className='nav-links onClick{closeMobileMenu}'>
+                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
                         Products
                     </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links onClick{closeMobileMenu}'>
+                    <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                         Sign up
                     </Link>
                 </li>
